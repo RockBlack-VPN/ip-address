427d83db5bd65099a1e09ba57a5f84506824a67b
RockBlack <kniazewdmitry@gmail.com>
2024-09-28 13:52:21 +0300

    Intel # Microsoft

---
+++ Global/Intel/Intel.bat
@@ -0,0 +1,6 @@
+route ADD 13.67.0.0 MASK 255.255.0.0 0.0.0.0  # Intel Corporation
+route ADD 15.0.0.0 MASK 255.0.0.0 0.0.0.0  # Intel Corporation
+route ADD 64.100.0.0 MASK 255.255.0.0 0.0.0.0  # Intel Corporation
+route ADD 132.163.0.0 MASK 255.255.0.0 0.0.0.0  # Intel Corporation
+route ADD 192.55.52.0 MASK 255.255.255.0 0.0.0.0  # Intel Corporation
+route ADD 198.145.0.0 MASK 255.255.0.0 0.0.0.0  # Intel Corporation
---
+++ Global/Microsoft/Microsoft.bat
@@ -0,0 +1,13 @@
+route ADD 13.64.0.0 MASK 255.224.0.0 0.0.0.0  # Microsoft Azure Cloud
+route ADD 20.36.0.0 MASK 255.252.0.0 0.0.0.0  # Microsoft
+route ADD 23.96.0.0 MASK 255.248.0.0 0.0.0.0  # Microsoft Azure
+route ADD 40.64.0.0 MASK 255.192.0.0 0.0.0.0  # Microsoft Global Network
+route ADD 52.96.0.0 MASK 255.240.0.0 0.0.0.0  # Microsoft Office 365
+route ADD 65.52.0.0 MASK 255.252.0.0 0.0.0.0  # Microsoft
+route ADD 104.40.0.0 MASK 255.248.0.0 0.0.0.0  # Microsoft
+route ADD 137.116.0.0 MASK 255.248.0.0 0.0.0.0  # Microsoft Azure
+route ADD 157.54.0.0 MASK 255.254.0.0 0.0.0.0  # Microsoft
+route ADD 168.61.0.0 MASK 255.255.0.0 0.0.0.0  # Microsoft Azure
+route ADD 191.232.0.0 MASK 255.252.0.0 0.0.0.0  # Microsoft Global Network
+route ADD 207.46.0.0 MASK 255.255.0.0 0.0.0.0  # Microsoft
+route ADD 204.79.195.0 MASK 255.255.255.0 0.0.0.0  # Microsoft
