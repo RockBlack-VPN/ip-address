e1173001c3dda67ed828fd03386bc62f6f951d14
RockBlack <98517320+RockBlack-VPN@users.noreply.github.com>
2026-02-12 23:00:02 +0300

    Update telegram.bat

--- Global/Telegram/telegram.bat
+++ Global/Telegram/telegram.bat
@@ -1,14 +1,22 @@
-#AS62041,AS59930,AS62014
-#Telegram Messenger Inc
+# AS62041, AS59930, AS62014
+# Telegram Messenger Inc
 route add 91.108.4.0 mask 255.255.252.0 0.0.0.0
 route add 91.108.8.0 mask 255.255.252.0 0.0.0.0
 route add 91.108.12.0 mask 255.255.252.0 0.0.0.0
 route add 91.108.16.0 mask 255.255.252.0 0.0.0.0
 route add 91.108.20.0 mask 255.255.252.0 0.0.0.0
 route add 91.108.56.0 mask 255.255.252.0 0.0.0.0
+route add 91.108.58.0 mask 255.255.254.0 0.0.0.0
 route add 95.161.64.0 mask 255.255.240.0 0.0.0.0
 route add 149.154.160.0 mask 255.255.240.0 0.0.0.0
 route add 149.154.162.0 mask 255.255.254.0 0.0.0.0
 route add 149.154.164.0 mask 255.255.254.0 0.0.0.0
 route add 149.154.166.0 mask 255.255.254.0 0.0.0.0
+route add 149.154.168.0 mask 255.255.252.0 0.0.0.0
+route add 149.154.172.0 mask 255.255.252.0 0.0.0.0
 route add 185.76.151.0 mask 255.255.255.0 0.0.0.0
+
+# Cloudflare + Digital Ocean
+route add 104.20.26.0 mask 255.255.255.0 0.0.0.0
+route add 172.66.170.0 mask 255.255.255.0 0.0.0.0
+route add 188.166.3.0 mask 255.255.255.0 0.0.0.0
