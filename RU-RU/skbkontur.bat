c23ed483d45e594375a49cdf025150ff5af032d4
Maxim <55232024+maximkasimov@users.noreply.github.com>
2026-02-24 22:39:37 +0300

    Create skbkontur.bat

---
+++ RU-RU/skbkontur.bat
@@ -0,0 +1,5 @@
+REM Kontur routes (bypass VPN)
+
+route add 46.17.200.0 mask 255.255.248.0 0.0.0.0
+route add 89.169.16.0 mask 255.255.252.0 0.0.0.0
+route add 185.161.180.0 mask 255.255.252.0 0.0.0.0
